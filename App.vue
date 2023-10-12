410884fc74af03c8c2b1e47274998132b7818329
Princeey <113655140+Princeey@users.noreply.github.com>
2023-10-12 17:59:20 +0800

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,20 @@
+<script>
+import Content from './components/Content.vue'
+import RegularB from './components/RegularB.vue'
+
+export default {
+ components: {
+  Content,
+  RegularB
+ }
+}
+</script>
+
+<template>
+  <Content />
+  <RegularB/>
+</template>
+
+<style scoped>
+
+</style>
